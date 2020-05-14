3fe7c7cd533dfe2d947c5355d5d11e83c37c8263
tribalwarsplayer <daniel.molnar0412@gmail.com>
2020-05-14 23:44:57 +0200

    early game scavenging

--- scav_pro_ui.js
+++ scav_pro_ui.js
@@ -14,57 +14,34 @@ async function run() {
     if (button.length >= 2) {
         await new Promise(r => setTimeout(r, 500));
         for (let i = button.length - 1; i > 0; --i) {
-           console.log('Click');
            await new Promise(r => setTimeout(r, 250));
            try {
                window.TwCheese.useTool('ASS').prepareBestOption();
            } catch {
                console.log('Prepared');
            }
+           console.log('Click');
            await new Promise(r => setTimeout(r, 250));
            button.eq(i).trigger("click");
         }
     }
     await new Promise(r => setTimeout(r, 250));
-    await getNextVillage();
+    window.partialReload();
     console.log('wait 5s');
 }
 
-async function getNextVillage() {
-    let html = link[0] + "n" + window.top.game_data.village.id + link[1];
-    console.log('get ' + html);
-    window.top.$.ajax({
-        type: "GET",
-        url: html,
-        dataType: "html",
-        error: function(e, t) {
-            alert("Error: " + t)
-        },
-        success: function(e) {
-            let t = window.top.$(e)
-              , o = /<\s*title\s*>([^<]+)<\/title\s*>/g.exec(e)[1]
-              , i = window.top.$.parseJSON(e.split("TribalWars.updateGameData(")[1].split(");")[0]);
-            window.top.game_data = i;
-            window.top.$("#header_info").html(window.top.$("#header_info", t).html()),
-            window.top.$("#topContainer").html(window.top.$("#topContainer", t).html()),
-            window.top.$("#contentContainer").html(window.top.$("#contentContainer", t).html()),
-            window.top.$("#quickbar_inner").html(window.top.$("#quickbar_inner", t).html()),
-            window.top.$("head").find("title").html(o);
-        }
-    });
-}
 async function scavenge() {
     await loadCheese();
     while(true) {
         let villages = parseInt(window.game_data.player.villages);
         let counter = 0;
-        while (counter < villages+1) {
+        while (counter < villages) {
             ++counter;
             await run();
             await new Promise(r => setTimeout(r, 5000));
         }
-        console.log('wait 5 min');
-        await new Promise(r => setTimeout(r, 5*60*1000));
+        console.log('wait 2 min');
+        await new Promise(r => setTimeout(r, 2*60*1000));
     }
 }
 
