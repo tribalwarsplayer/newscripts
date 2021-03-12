852927ae2b8559909990cd8ee8b04b18b858e65f
tribalwarsplayer <63425603+tribalwarsplayer@users.noreply.github.com>
2021-03-12 23:07:31 +0100

    Update scav_smart_single.js

--- scav_smart_single.js
+++ scav_smart_single.js
@@ -53,7 +53,9 @@ async function scavenge() {
     await loadCheese();
     let villages = parseInt(window.game_data.player.villages);
     let hasOneVillage = (villages == 1);
-    if (!hasOneVillage) break;
+    if (!hasOneVillage) {
+        break;
+    }
     while(true) {
       debugger;
       let html_collection = document.getElementsByClassName("return-countdown");
