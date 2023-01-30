7b8d7ccfb65822f60f6fb5828f8abe2b604d03ca
Daniel Molnar <molnard0412@gmail.com>
2023-01-29 21:19:43 -0500

    fix typo

--- sort.js
+++ sort.js
@@ -7,7 +7,7 @@ const AT_PLUS_SPACE = 6;
 
 let message_box = document.getElementById(`message_${message_id}`);
 
-let attacks_per_line = memo.innerHTML.split('\n');
+let attacks_per_line = message_box.innerHTML.split('\n');
 let time_of_attacks = attacks_per_line.map(line => {
   let at = line.indexOf("ekkor");
   return [new Date(line.slice(at+AT_PLUS_SPACE)), line];
@@ -16,5 +16,5 @@ let time_of_attacks = attacks_per_line.map(line => {
 //sort based on time
 time_of_attacks.sort((a, b) => a[0] < b[0]);
 let note = time_of_attacks.map(e => e[1]).join('\n');
-memo.innerHTML = note;
+message_box.innerHTML = note;
 
