58767022ff45e6a5b8e16762927e8cfc07bf1e5e
Daniel Molnar <molnard0412@gmail.com>
2023-01-29 21:16:52 -0500

    add sorting for attack in note

---
+++ sort.js
@@ -0,0 +1,20 @@
+Memo.toggleEdit()
+
+//get id of current tab
+let message_id = $('.memo-tab-selected')[0].id.split('_')[1]
+
+const AT_PLUS_SPACE = 6;
+
+let message_box = document.getElementById(`message_${message_id}`);
+
+let attacks_per_line = memo.innerHTML.split('\n');
+let time_of_attacks = attacks_per_line.map(line => {
+  let at = line.indexOf("ekkor");
+  return [new Date(line.slice(at+AT_PLUS_SPACE)), line];
+});
+
+//sort based on time
+time_of_attacks.sort((a, b) => a[0] < b[0]);
+let note = time_of_attacks.map(e => e[1]).join('\n');
+memo.innerHTML = note;
+
